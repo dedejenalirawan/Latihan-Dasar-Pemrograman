eecbec3473152950461de5a4ed3dcd598e7eb5f0
dedejenalirawan <114629929+dedejenalirawan@users.noreply.github.com>
2022-09-29 11:14:40 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,14 @@
+#aplikasi biodata create by Dede
+
+#memasukan nama
+print("Yuk! Lengkapi data diri ..")
+
+#input data diri
+nama = input ("Nama                 : ")
+kelas = input("kelas                : ")
+prodi = input("Program studi        : ")
+
+# print / output nama
+print("Nama anda adalah         : ", nama)
+print("Kelas anda adalah        : ", kelas)
+print("Program studi anda adalah: ", prodi)
