9e48bdb6c528745d3ee10896cf87fcfa4bded880
dedejenalirawan <114629929+dedejenalirawan@users.noreply.github.com>
2022-10-06 09:19:15 +0700

    tugasSesi3

---
+++ tugasSesi3.py
@@ -0,0 +1,6 @@
+Nama    ='Dede jenal irawan'
+Kelas   = 'TI22A'
+Jurusan = 'Teknik Informatika'
+Quote   = 'Perjuangan sepanjang syareat, Hasil sepanjang hakekat'
+
+print(Nama, Kelas, Jurusan, Quote)
